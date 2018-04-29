d06b05e1bc5e6a7c7bdd5a0db5cc26526f893fc1
lenka <sudhanshulenka@ymail.com>
2018-04-30 01:54:16 +0530

    changes for emergency services

--- src/app/home/emergency/emergency.component.ts
+++ src/app/home/emergency/emergency.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmergencySearchService } from './emergency.service';
+import { Routes, RouterModule, Router, ActivatedRoute } from "@angular/router";
+
+
 
 @Component({
   selector: 'app-emergency-search',
@@ -9,11 +12,36 @@ import { EmergencySearchService } from './emergency.service';
 })
 export class EmergencySearchComponent implements OnInit {
 
+  emergencySerSearchCount:Array<Object>=new Array<Object>() ;
+  emergencyTypeCount:Map<string, Array<number>> = new Map<string, Array<number>>();
+  latitude:number=37.793461;
+  longitude:number=-122.399642;
 
-  constructor(private emergencySearchService: EmergencySearchService) {
+
+  constructor(
+    private emergencySearchService: EmergencySearchService, 
+    private route: ActivatedRoute,
+    private router: Router) {
+      this.emergencyServiceTypeCount();
    }
 
-  ngOnInit() {
+   emergencyServiceTypeCount(){
+    this.emergencySearchService.getEmergencyServicesDetails().subscribe(data =>{
+      this.emergencyTypeCount=data;
+      Object.keys(this.emergencyTypeCount).forEach(key=> {
+        let  keyvalue:  Array<number>;
+        keyvalue= this.emergencyTypeCount[key]  ;  
+       var activityCount={};
+       activityCount["name"]=key;
+       activityCount["count"]=keyvalue.length;
+       activityCount["ids"]=keyvalue;
+         this.emergencySerSearchCount.push(activityCount);
+        });
+   });
+
+   }
+
+   ngOnInit() {
     
   }
 
--- src/app/home/emergency/emergency.service.ts
+++ src/app/home/emergency/emergency.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
+import { Http,Headers,RequestOptions,Response,RequestMethod,Request} from '@angular/http';
+import { Observable } from 'rxjs';
 @Injectable()
 export class EmergencySearchService {
 
+
+    constructor(private http :Http) {
+        
+       }
+
+
+       getEmergencyServicesDetails() {
+        let postheaders = new Headers({'Content-Type': 'application/json','Access-Control-Allow-Origin':'*'});
+        let options = new RequestOptions({ headers: postheaders });
+       return this.http.get('assets/data/emergencyServices.json',options).map(res => res.json());
+      }
 }
